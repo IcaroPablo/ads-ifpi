1883304b599779b7a68fc9a096ad02f056c3508b
Ícaro Andrade <ic4ro.p4blo@gmail.com>
2022-10-14 10:26:05 -0300

    correção do algoritmo de lista

--- EDD/listas/lista_int.cpp
+++ EDD/listas/lista_int.cpp
@@ -1,28 +1,32 @@
 #include <stdio.h>
-#include <iostream>
-using namespace std;
+#include <string.h>
 
-typedef struct aluno {
-	int mat;
-	string nome;	
-} Aluno; 
+typedef struct item {
+	int id;
+	char valor[50];	
+} Item; 
 
-Aluno lista[30];
+Item list[30];
 
-void incluirDesordenado(Aluno e) {
+int maximo = sizeof(list)/sizeof(list[0]);
 
-	int qa=0; int cont;
-	printf("\nInserir Novo Aluno\n\n");
+int qa = 0;
+
+void incluirDesordenado(Item e) {
+
+	int cont;
+
+	printf("\nInserir Novo Item:");
 	do {
-		if (qa < maximo) { // verifica se o vetor pode receber novo aluno
-			printf("\nMatricula do Aluno: ");
-			scanf("%d",&turma[qa].mat);
-			printf("\nNome: ");
-			scanf("%s",turma[qa].nome);
+		if (qa < maximo) {
+			printf("\ninforme o id do item: ");
+			scanf("%d",&list[qa].id);
+			printf("\ninforme o valor do item: ");
+			scanf("%s",list[qa].valor);
 			qa++;
-			printf("\n\nAluno Inserido com Sucesso!!!\n\n");
-		} else { // vetor cheio
-			printf("\n\n Nao Pode Inserir – Turma Cheia!!!\n\n");
+			printf("\n\nItem Inserido com Sucesso!!!\n");
+		} else { 
+			printf("\n\n Nao Pode Inserir - lista cheia!!!\n");
 			break;
 		}
 		printf("\n\n Inserir outro(1-sim/2-nao)? ");
@@ -31,31 +35,91 @@ void incluirDesordenado(Aluno e) {
 
 }
 
-//retorna a posicao do elemento procurado
-int procura(int mat) {	
-
+int procura(int id) {	
+	for(int i = 0; i < maximo; i++) {
+		if(list[i].id == id) {
+			return i;
+		}
+	}
+	return -1;
 }
 
-//recebe a posicao e imprime o elemento na tela
 void mostrar(int pos) {
 
+	if(pos < maximo) {
+		printf("elemento: %d\nid: %d\nvalor: %s", pos, list[pos].id, list[pos].valor);
+	}
+
 }
 
-//procura o elemento e depois mostra o elemento encontrado (se nao for encontrado informa q ele nao existe)
-void consultar(int mat) {	
+void consultar(int id) {	
+	int pos = procura(id);
+	if(pos > -1) {
+		mostrar(pos);
+	} else {
+		printf("\nelemento não existe");
+	}
+}
+
+void remover() {
+
+	int matprocurada, i, cont, achou, resp;
+	do {
+		printf("\nRemover Item:");
+		printf("\nId do Item: ");
+		scanf("%d",&matprocurada);
+
+		achou = procura(matprocurada);
+		if (achou != -1) {
+			mostrar(achou);
+			printf("\nDeseja remover o item (1-sim/2-nao)? ");
+			scanf("%d",&resp);
+
+			if (resp==1) {
+				remover00(achou);
+				// remover01(achou);
+
+				printf("\n\nitem removido com Sucesso!!!\n");
+			} else {
+				printf("\n\n\aO item nao foi removido!!!\n");
+				break;
+			}
+		} else {
+			printf("\n\naNumero de id Incorreto!!!!!!\n");
+		}
+
+		printf("\n\nRemover outro(1-sim/2-nao)? ");
+		scanf("%d",&cont);
+	} while (cont == 1);
 
 }
 
 //estrat�gia 1: colocar o ultimo elemento da lista na posi��o do elemento removido
-void remover00() {
+void remover00(int pos) {
+
+	list[pos] = list[qa];
+	qa--;
 
 }
 
 //estrat�gia 2: mover TODOS os elementos que est�o AP�S  o elemento que deve ser removido UMA POSI��O A FRENTE.
-void remover01() {
+void remover01(int pos) {
+
+	if(pos == maximo-1) qa--;
+	else {
+		for(int i = pos; i <= qa; i++) {
+			list[i] = list[i+1];
+		}
+	}
+
+	qa--;
 
 }
 
-main() {
+int main() {
+	strcpy(list[0].valor, "oi");
+	printf("%s", list[0].valor);
+	printf("%s", list[1].valor);
 
+	return 0;
 }
